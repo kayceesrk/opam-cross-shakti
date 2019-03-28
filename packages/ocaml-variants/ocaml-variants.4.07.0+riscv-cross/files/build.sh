25f234484042860e38ed08dbe30effacbbf49ffd
KC Sivaramakrishnan <kc@kcsrk.info>
2019-03-28 13:39:24 +0000

    edits

---
+++ packages/ocaml-variants/ocaml-variants.4.07.0+riscv-cross/files/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./configure -no-ocamldoc -no-debugger -prefix %{build}%/install
+make -j world.opt
+make install
+make clean
+./configure --target riscv64-unknown-linux-gnu -no-ocamldoc -no-debugger -target-bindir %{build}%/install/bin
+make -j4 world || /bin/true
+make -j4 opt
